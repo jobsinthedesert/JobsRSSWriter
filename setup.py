ba1bf6aef111737139013ad0a7fa3e96884f4196
jobsinthedesert <jobsinthedesert@gmail.com>
2018-12-19 23:55:45 -0800

    Writing JobsRSSWriter.py as an individual package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='JobsRSSWriter',
+      version='1.0',
+      description='Cleans data to create RSS feed',
+      author='Jobs in the Desert',
+      author_email='jobsinthedesert@gmail.com',
+      url='https://github.com/jobsinthedesert',
+      py_modules=['JobsRSSWriter']
+      )
